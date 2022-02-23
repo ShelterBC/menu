ef8f83eb5bcf505779742289d62393550687d5c1
ShelterBC <99732629+ShelterBC@users.noreply.github.com>
2022-02-23 17:06:03 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,37 @@
-$(document).ready(function(){
+$('#bavarages1').click(slidersAppear);
+$('#navvCoff').click(slidersAppear);
+function slidersAppear () {
+    var newElems = $("<div id = 'addOne' class='slider'>" +
+    "<div class='slider__item'> <img class='productImage' src='media/cofee/espresso.jpeg'> </div>" +
+    "<div class='slider__item'><img class='productImage' src='media/cofee/coffee-americano.jpg'></div>" + 
+    "<div class='slider__item'><img class='productImage' src='media/cofee/espVsAmer.jpg'></div></div>"
+    +"<div id = 'addOneo' class='productDescription'><p style = 'text-align: left;''>Еспресо, Американо</p><hr></div>");
+    $('#fSlider').append(newElems);
+
+
+    newElems = $("<div id = 'addTwo' class='slider'>" +
+    "<div class='slider__item'> <img class='productImage' src='media/cofee/capuch.jpg'> </div>" +
+    "<div class='slider__item'><img class='productImage' src='media/cofee/latte.jpg'></div>" + 
+    "<div class='slider__item'><img class='productImage' src='media/cofee/flet.jpeg'></div>" + 
+    "<div class='slider__item'><img class='productImage' src='media/cofee/capVslatte.jpg'></div></div>"
+    +"<div id = 'addTwot' class='productDescription'><p style = 'text-align: left;''>Капучіно, Лате, Флет уайт</p><hr></div>");
+    $('#sSlider').append(newElems);
+
+    $(document).ready(function(){
     $('.slider').slick({
         arrows:false,
-        dots:true,
-        speed: 1000,
-        slidesToShow:1,
-        slidesToScroll:1
+        dots:true
     });
   });
+  }
+
+  $('#backk').click(slidersRemove);
+    $('#navvLan').click(slidersRemove);
+    $('#navvSnid').click(slidersRemove);
+    $('#navvTea').click(slidersRemove);
+    function slidersRemove() {
+        $('#addOne').remove();
+        $('#addOneo').remove();
+        $('#addTwo').remove();
+        $('#addTwot').remove();
+  }

2d2390ba4156d6f8db903644d427836ce5be9645
ShelterBC <99732629+ShelterBC@users.noreply.github.com>
2022-04-17 11:47:29 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -14,10 +14,10 @@ function slidersAppear () {
     +"<div id = 'addTwot' class='productDescription'><p style = 'text-align: left;''>Капучіно, Лате</p><hr></div>");
     $('#sSlider').append(newElems);
     
-    newElems = $("<div id = 'addTwo' class='slider'>" +
-    "<div class='slider__item'> <img class='productImage' src='media/cofee/capuch.jpg'> </div>" +
-    "<div class='slider__item'><img class='productImage' src='media/cofee/latte.jpg'></div></div>"
-    +"<div id = 'addTwot' class='productDescription'><p style = 'text-align: left;''>Флет уайт, какао</p><hr></div>");
+    newElems = $("<div id = 'addThree' class='slider'>" +
+    "<div class='slider__item'> <img class='productImage' src='media/cofee/flet.jpeg'> </div>" +
+    "<div class='slider__item'><img class='productImage' src='media/cofee/cacao.png'></div></div>"
+    +"<div id = 'addThreeO' class='productDescription'><p style = 'text-align: left;''>Флет уайт, Какао</p><hr></div>");
     $('#bSlider').append(newElems);
 
     $(document).ready(function(){
@@ -29,12 +29,15 @@ function slidersAppear () {
   }
 
   $('#backk').click(slidersRemove);
-    $('#navvLan').click(slidersRemove);
-    $('#navvSnid').click(slidersRemove);
-    $('#navvTea').click(slidersRemove);
+  $('#navvLan').click(slidersRemove);
+  $('#navvSnid').click(slidersRemove);
+  $('#navvTea').click(slidersRemove);
+  $('#navvCoctels').click(slidersRemove);
     function slidersRemove() {
         $('#addOne').remove();
         $('#addOneo').remove();
         $('#addTwo').remove();
         $('#addTwot').remove();
+        $('#addThree').remove();
+        $('#addThreeO').remove();
   }

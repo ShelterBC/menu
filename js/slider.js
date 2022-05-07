b739d51bbcae100cd1abfc0d690faeb899e5ff05
ShelterBC <99732629+ShelterBC@users.noreply.github.com>
2022-05-07 15:48:25 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,26 @@
 $('#bavarages1').click(slidersAppear);
 $('#navvCoff').click(slidersAppear);
 function slidersAppear () {
-    var newElems = $("<div id = 'addOne' class='slider'>" +
+    var newElems = $("<div data-aos='fade-up' id = 'addOne' class='slider'>" +
     "<div class='slider__item'> <img class='productImage' src='media/cofee/espresso.jpeg'> </div>" +
     "<div class='slider__item'><img class='productImage' src='media/cofee/coffee-americano.jpg'></div></div>"
     +"<div id = 'addOneo' class='productDescription'><p style = 'text-align: left;''>Еспресо, Американо</p><hr></div>");
     $('#fSlider').append(newElems);
 
 
-    newElems = $("<div id = 'addTwo' class='slider'>" +
+    newElems = $("<div data-aos='fade-up' id = 'addTwo' class='slider'>" +
     "<div class='slider__item'> <img class='productImage' src='media/cofee/capuch.jpg'> </div>" +
     "<div class='slider__item'><img class='productImage' src='media/cofee/latte.jpg'></div></div>"
     +"<div id = 'addTwot' class='productDescription'><p style = 'text-align: left;''>Капучіно, Лате</p><hr></div>");
     $('#sSlider').append(newElems);
     
-    newElems = $("<div id = 'addThree' class='slider'>" +
+    newElems = $("<div data-aos='fade-up' id = 'addThree' class='slider'>" +
     "<div class='slider__item'> <img class='productImage' src='media/cofee/flet.jpeg'> </div>" +
     "<div class='slider__item'><img class='productImage' src='media/cofee/cacao.png'></div></div>"
     +"<div id = 'addThreeO' class='productDescription'><p style = 'text-align: left;''>Флет уайт, Какао</p><hr></div>");
     $('#bSlider').append(newElems);
+
+    AOS.refresh();
 
     $(document).ready(function(){
     $('.slider').slick({
